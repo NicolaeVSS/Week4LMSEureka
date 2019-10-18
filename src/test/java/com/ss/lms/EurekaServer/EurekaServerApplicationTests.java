e74d83b672a1cbc2b4a486afe4476f80028cc13a
NicolaeVSS <nicolae.vasile@smoothstack.com>
2019-10-17 21:05:54 -0400

    dependencies

--- src/test/java/com/ss/lms/EurekaServer/EurekaServerApplicationTests.java
+++ src/test/java/com/ss/lms/EurekaServer/EurekaServerApplicationTests.java
@@ -2,7 +2,9 @@ package com.ss.lms.EurekaServer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootTest
 class EurekaServerApplicationTests {
 
